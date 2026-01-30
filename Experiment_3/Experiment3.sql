1a4b17153ff50dc7b35d0ee540cd56003d921815
Z3ro-Kun <132244480+Z3ro-Kun@users.noreply.github.com>
2026-01-30 10:12:39 +0530

    Update Experiment3.sql

--- Experiment_3/Experiment3.sql
+++ Experiment_3/Experiment3.sql
@@ -1,1 +1,8 @@
-
+DECLARE
+emp_id NUMBER:=101;
+emp_name VARCHAR2(20):='Vinay';
+emp_salary NUMBER:=10000;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Employee ID: ' || emp_id || ', Employee Name: ' || emp_name || ', Employee Salary: ' || emp_salary);
+    DBMS_OUTPUT.PUT_LINE('Bonus: '|| emp_salary *0.05);
+END;
